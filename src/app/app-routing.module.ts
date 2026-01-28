6e97adfc1f321ced4a1dc9b0c66a5be5e3b09c5b
UdayMayanna <mayannauday9@gmail.com>
2026-01-28 10:38:01 +0530

    Added page not found route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,16 @@ import { RouterModule, Routes } from '@angular/router';
 import { TodoComponent } from './comp/todo/todo.component';
 import { InterpolationComponent } from './comp/interpolation/interpolation.component';
 import { TwoWayBindingComponent } from './comp/two-way-binding/two-way-binding.component';
+import { PageNotFoundComponent } from './comp/page-not-found/page-not-found.component';
 
 const routes: Routes = [
   {path: '', redirectTo: 'home', pathMatch: 'full' },
   {path:'home', component:TodoComponent},
   {path:'interpollation', component:InterpolationComponent},
-  {path:'twowaybinding', component:TwoWayBindingComponent}
+  {path:'twowaybinding', component:TwoWayBindingComponent},
+  {path:'**',component:PageNotFoundComponent}
+  //Also called as wild card route
+  // Always last route
 ];
 
 @NgModule({
